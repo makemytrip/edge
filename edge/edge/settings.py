133c21c811fbe7403e973fd41c0eb6360f3da33e
Paritosh Anand <Paritosh.Anand@makemytrip.com>
2019-02-04 10:54:11 +0530

    new hash key

--- edge/edge/settings.py
+++ edge/edge/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kv&d-(91_butn6c!^qxpqy^c^#zy$28bj5p148#$^c^uctwd2e'
+SECRET_KEY = '<SECRET_KEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
